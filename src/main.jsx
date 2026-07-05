0e8fbe2875c8fc878ad21141862860454cf414c5
NUKUL LILHARE <128592344+nukul6446@users.noreply.github.com>
2026-07-05 22:55:30 +0530

    structure version of src

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-const rootEl = document.getElementById("root");
-ReactDOM.createRoot(rootEl).render(<App />);
